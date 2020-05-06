2bd52a4b8809f839219ba4bff6f867a0a6eec07c
Cezary Jakubowski <cezary.jakubowski@infullmobile.com>
2020-05-06 11:16:11 +0200

    Initial commit - In the beninging ther was smolProject

---
+++ ios/Pods/Target Support Files/Pods-smolProject-tvOS/Pods-smolProject-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smolProject_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_smolProject_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-smolProject-tvOSTests/Pods-smolProject-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smolProject_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_smolProject_tvOSTests
+@end
